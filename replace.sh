ed84f473e2af206c0e01f9181d8ce077dbb3d179
Ben Jacobs <benmillerj@gmail.com>
2024-02-26 17:30:23 -0500

    focus on csv for replacements

--- replace.sh
+++ replace.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 while IFS=: read -r old new; do
-  find . -type f -name "*.txt" -print0 | xargs -0 sed -i '' "s/$old/$new/g"
+  find . -type f -name "*.csv" -print0 | xargs -0 sed -i '' "s/$old/$new/g"
 done < replacements.txt
