975915ed8964149a0a8844053dafab1d7934fe20
Ben Jacobs <benmillerj@gmail.com>
2024-02-27 17:20:24 -0500

    fixed up banner. also, apparently that _is_ only a warning and i got confused about what headers i needed to delete

--- replace.sh
+++ replace.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(dirname "$0")
+
 while IFS=: read -r old new; do
   find . -type f -name "*.csv" -print0 | xargs -0 sed -i '' "s/$old/$new/g"
-done < replacements.txt
+done < "$SCRIPT_DIR/replacements.txt"
